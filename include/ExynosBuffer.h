fa529db6d9bff4d045d10af184a0fb8d706460af
Sangwoo Park <sw5771.park@samsung.com>
2012-11-27 12:51:29 +0900

    include: make buffer size as 4
    Change-Id: Ifa05e2c559a86a5b723155d41c8b8c910a6acfc9
    Signed-off-by: Sangwoo Park <sw5771.park@samsung.com>

--- include/ExynosBuffer.h
+++ include/ExynosBuffer.h
@@ -27,6 +27,9 @@
  * - 2012/03/14 : sangwoo.park(sw5771.park@samsung.com) \n
  *   Change file, struct name to ExynosXXX.
  *
+ * - 2012/10/08 : sangwoo.park(sw5771.park@samsung.com) \n
+ *   Add BUFFER_PLANE_NUM_DEFAULT, and, Increase Buffer as 4.
+ *
  */
 
 #ifndef EXYNOS_BUFFER_H_
@@ -45,77 +48,83 @@ public:
     enum BUFFER_TYPE
     {
         BUFFER_TYPE_BASE     = 0,
-        BUFFER_TYPE_VIRT     = 1,      //!< virtual address
+        BUFFER_TYPE_VIRT     = 1 << 0, //!< virtual address
         BUFFER_TYPE_PHYS     = 1 << 1, //!< physical address
-        BUFFER_TYPE_RESERVED = 1 << 2, //!< reserved type
-        BUFFER_TYPE_FD       = 1 << 3, //!< physical address
+        BUFFER_TYPE_FD       = 1 << 2, //!< fd address
+        BUFFER_TYPE_RESERVED = 1 << 3, //!< reserved type
         BUFFER_TYPE_MAX,
+    };
+
+    //! Buffer plane number
+    enum BUFFER_PLANE_NUM
+    {
+        BUFFER_PLANE_NUM_DEFAULT = 4,
     };
 
     //! Buffer virtual address
     union {
         char *p;       //! single address.
-        char *extP[3]; //! Y Cb Cr.
+        char *extP[BUFFER_PLANE_NUM_DEFAULT]; //! Y Cb Cr.
     } virt;
 
     //! Buffer physical address
     union {
         unsigned int p;       //! single address.
-        unsigned int extP[3]; //! Y Cb Cr.
+        unsigned int extP[BUFFER_PLANE_NUM_DEFAULT]; //! Y Cb Cr.
     } phys;
 
     //! Buffer file descriptors
     union {
-	int fd;
-	int extFd[3];
+        int fd;       //! single address.
+        int extFd[BUFFER_PLANE_NUM_DEFAULT]; //! Y Cb Cr.
     } fd;
 
     //! Buffer reserved id
     union {
         unsigned int p;       //! \n
-        unsigned int extP[3]; //! \n
+        unsigned int extP[BUFFER_PLANE_NUM_DEFAULT]; //! \n
     } reserved;
 
     //! Buffer size
     union {
         unsigned int s;
-        unsigned int extS[3];
+        unsigned int extS[BUFFER_PLANE_NUM_DEFAULT];
     } size;
 
 #ifdef __cplusplus
     //! Constructor
     ExynosBuffer()
     {
-        for (int i = 0; i < 3; i++) {
-            virt.    extP[i] = NULL;
-            phys.    extP[i] = 0;
-	    fd.      extFd[i] = -1;
-            reserved.extP[i] = 0;
-            size.    extS[i] = 0;
+        for (int i = 0; i < BUFFER_PLANE_NUM_DEFAULT; i++) {
+            virt.    extP [i] = NULL;
+            phys.    extP [i] = 0;
+            fd.      extFd[i] = -1;
+            reserved.extP [i] = 0;
+            size.    extS [i] = 0;
         }
     }
 
     //! Constructor
     ExynosBuffer(const ExynosBuffer *other)
     {
-        for (int i = 0; i < 3; i++) {
-            virt.    extP[i] = other->virt.extP[i];
-            phys.    extP[i] = other->phys.extP[i];
-	    fd.      extFd[i] = other->fd.extFd[i];
-            reserved.extP[i] = other->reserved.extP[i];
-            size.    extS[i] = other->size.extS[i];
+        for (int i = 0; i < BUFFER_PLANE_NUM_DEFAULT; i++) {
+            virt.    extP [i] = other->virt.extP[i];
+            phys.    extP [i] = other->phys.extP[i];
+            fd.      extFd[i] = other->fd.extFd[i];
+            reserved.extP [i] = other->reserved.extP[i];
+            size.    extS [i] = other->size.extS[i];
         }
     }
 
     //! Operator(=) override
     ExynosBuffer& operator =(const ExynosBuffer &other)
     {
-        for (int i = 0; i < 3; i++) {
-            virt.    extP[i] = other.virt.extP[i];
-            phys.    extP[i] = other.phys.extP[i];
-	    fd.      extFd[i] = other.fd.extFd[i];	
-            reserved.extP[i] = other.reserved.extP[i];
-            size.    extS[i] = other.size.extS[i];
+        for (int i = 0; i < BUFFER_PLANE_NUM_DEFAULT; i++) {
+            virt.    extP [i] = other.virt.extP[i];
+            phys.    extP [i] = other.phys.extP[i];
+            fd.      extFd[i] = other.fd.extFd[i];
+            reserved.extP [i] = other.reserved.extP[i];
+            size.    extS [i] = other.size.extS[i];
         }
         return *this;
     }
@@ -123,21 +132,20 @@ public:
     //! Operator(==) override
     bool operator ==(const ExynosBuffer &other) const
     {
-        return (   virt.    extP[0] == other.virt.extP[0]
-                && virt.    extP[1] == other.virt.extP[1]
-                && virt.    extP[2] == other.virt.extP[2]
-                && phys.    extP[0] == other.phys.extP[0]
-                && phys.    extP[1] == other.phys.extP[1]
-                && phys.    extP[2] == other.phys.extP[2]
-                && fd.      extFd[0] == other.fd.extFd[0]
-                && fd.      extFd[1] == other.fd.extFd[1]
-                && fd.      extFd[2] == other.fd.extFd[2]
-                && reserved.extP[0] == other.reserved.extP[0]
-                && reserved.extP[1] == other.reserved.extP[1]
-                && reserved.extP[2] == other.reserved.extP[2]
-                && size.    extS[0] == other.size.extS[0]
-                && size.    extS[1] == other.size.extS[1]
-                && size.    extS[2] == other.size.extS[2]);
+        bool ret = true;
+
+        for (int i = 0; i < BUFFER_PLANE_NUM_DEFAULT; i++) {
+            if (   virt.    extP [i] != other.virt.    extP[i]
+                || phys.    extP [i] != other.phys.    extP[i]
+                || fd.      extFd[i] != other.fd.      extFd[i]
+                || reserved.extP [i] != other.reserved.extP[i]
+                || size.    extS [i] != other.size.    extS[i]) {
+                ret = false;
+                break;
+            }
+        }
+
+        return ret;
     }
 
     //! Operator(!=) override
@@ -155,8 +163,8 @@ public:
             type |= BUFFER_TYPE_VIRT;
         if (buf->phys.p)
             type |= BUFFER_TYPE_PHYS;
-	if (buf->fd.fd >= 0)
-            type |= BUFFER_TYPE_FD;		
+        if (buf->fd.fd >= 0)
+            type |= BUFFER_TYPE_FD;
         if (buf->reserved.p)
             type |= BUFFER_TYPE_RESERVED;
 

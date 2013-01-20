f7b5f059d072f5374d23ba88df08f8a53f824b1e
Huisung Kang <hs1218.kang@samsung.com>
2013-01-21 07:29:05 +0900

    libhwc: Fixup compile error due to dependency of libfimg
    Change-Id: I6d411fd1b337adc0a5044faaaae4cb087eaeeac2
    Signed-off-by: Huisung Kang <hs1218.kang@samsung.com>

--- libhwc/Android.mk
+++ libhwc/Android.mk
@@ -21,7 +21,12 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libEGL libGLESv1_CM libhardware \
 	libhardware_legacy libion_exynos libutils libsync libexynosgscaler \
-	libexynosv4l2 libMcClient libexynosutils libfimg
+	libexynosv4l2 libMcClient libexynosutils
+
+ifeq ($(BOARD_USES_SKIA_FIMGAPI),true)
+LOCAL_SHARED_LIBRARIES += libfimg
+endif
+
 ifeq ($(BOARD_USES_HWC_SERVICES),true)
 	LOCAL_SHARED_LIBRARIES += libExynosHWCService
 	LOCAL_CFLAGS += -DHWC_SERVICES

87640b8d12c97b4094fd818af8f3c438ff1eb35f
Won Jongbin <jongbin.won@samsung.com>
2013-02-01 13:16:53 +0900

    libsecurepath: update libsecure path trustlet connector
    G2D src&dst physical address gets from ion fd in secmem
    driver.
    
    Change-Id: Id2cb7e81ce5e10e8b4c76f736e6b9edb98c87845
    Signed-off-by: Won Jongbin <jongbin.won@samsung.com>

--- libsecurepath/sec_g2ddrm.cpp
+++ libsecurepath/sec_g2ddrm.cpp
@@ -14,7 +14,12 @@
  * limitations under the License.
  */
 
+#include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
 
 #include "tlsecdrm_api.h"
 #define LOG_TAG "sec_g2ddrm"
@@ -22,7 +27,17 @@
 #include "tlc_communication.h"
 #include "sec_g2ddrm.h"
 
+#define SMEM_PATH	"/dev/s5p-smem"
+#define SECMEM_IOC_GET_FD_PHYS_ADDR _IOWR('S', 8, struct secfd_info)
+#define ion_phys_addr_t unsigned long
+
+struct secfd_info {
+	int fd;
+	ion_phys_addr_t phys;
+};
+
 mc_comm_ctx ctx;
+int g_fd_secmem;
 
 static mcResult_t tlc_initialize(void) {
 	mcResult_t mcRet;
@@ -60,6 +75,20 @@ static mcResult_t tlc_terminate(void) {
 	return MC_DRV_OK;
 }
 
+int get_fd_phyaddr_from_kernel(struct secfd_info *secfd)
+{
+	int ret;
+
+	ret = ioctl(g_fd_secmem, SECMEM_IOC_GET_FD_PHYS_ADDR, secfd);
+	if (ret != 0) {
+		LOG_E("Fail to get SECFD info: ret(%d)", ret);
+		return -1;
+	}
+
+	LOG_I("___ION_FD_KERNEL::fd(%d), phyaddr(0x%x)", secfd->fd, secfd->phys);
+	return 0;
+}
+
 extern "C" g2ddrmResult_t G2DDRM_Initialize(void)
 {
 	mcResult_t mcRet;
@@ -70,10 +99,18 @@ extern "C" g2ddrmResult_t G2DDRM_Initialize(void)
 	do {
 		LOG_I("Open the Trustlet");
 
+		g_fd_secmem = open(SMEM_PATH, O_RDWR);
+		if (g_fd_secmem < 0) {
+			LOG_E("open S5P-MEM device error");
+			ret = G2DDRM_ERROR_INIT_FAILED;
+			break;
+		}
+
 		mcRet = tlc_initialize();
 		if (MC_DRV_OK != mcRet) {
 			LOG_E("Tlc Open Error");
 			ret = G2DDRM_ERROR_INIT_FAILED;
+			close(g_fd_secmem);
 			break;
 		}
 
@@ -82,6 +119,7 @@ extern "C" g2ddrmResult_t G2DDRM_Initialize(void)
 		if (NULL == tci) {
 			LOG_E("TCI has not been set up properly - exiting");
 			ret = G2DDRM_ERROR_INIT_FAILED;
+			close(g_fd_secmem);
 			break;
 		}
 
@@ -91,12 +129,14 @@ extern "C" g2ddrmResult_t G2DDRM_Initialize(void)
 		if (MC_DRV_OK != mcRet) {
 			LOG_E("tlc_communicate Error!");
 			ret = G2DDRM_ERROR_INIT_FAILED;
+			close(g_fd_secmem);
 			break;
 		}
 
 		if ((RSP_ID(CMD_G2DDRM_INITIALIZE) != tci->resp.id)) {
 			LOG_E("Trustlet did not send a response : %d", tci->resp.id);
 			ret = G2DDRM_ERROR_INIT_FAILED;
+			close(g_fd_secmem);
 			break;
 		}
 		LOG_I("Trustlet response is completed");
@@ -104,6 +144,7 @@ extern "C" g2ddrmResult_t G2DDRM_Initialize(void)
 		if (tci->resp.return_code != RET_TL_G2DDRM_OK) {
 			LOG_E("Trustlet did not send a valid return code : %d", tci->resp.return_code);
 			ret = G2DDRM_ERROR_INIT_FAILED;
+			close(g_fd_secmem);
 			break;
 		}
 		LOG_I("Check the Trustlet return code is completed");
@@ -121,10 +162,27 @@ extern "C" g2ddrmResult_t G2DDRM_Blit(struct fimg2d_blit_raw *cmd)
 	g2ddrmResult_t ret = G2DDRM_SUCCESS;
 	mcBulkMap_t mapInfo;
 	struct tciMessage_t *tci = NULL;
+	struct secfd_info secfd;
 
 	mapInfo.sVirtualAddr = NULL;
 
 	do {
+		secfd.fd = cmd->src.addr.start;
+		if (get_fd_phyaddr_from_kernel(&secfd) < 0) {
+			LOG_E("fail to get phyaddr from fd");
+			ret = G2DDRM_ERROR_BLIT_FAILED;
+			break;
+		}
+		cmd->src.addr.start = secfd.phys;
+
+		secfd.fd = cmd->dst.addr.start;
+		if (get_fd_phyaddr_from_kernel(&secfd) < 0) {
+			LOG_E("fail to get phyaddr from fd");
+			ret = G2DDRM_ERROR_BLIT_FAILED;
+			break;
+		}
+		cmd->dst.addr.start = secfd.phys;
+
 		tci->cmd.id = CMD_G2DDRM_BLIT;
 		tci->blit.op = cmd->op;
 		tci->blit.param = cmd->param;
@@ -203,6 +261,7 @@ extern "C" g2ddrmResult_t G2DDRM_Terminate(void)
 
 		LOG_I("Check the Trustlet return code is completed");
 
+		close(g_fd_secmem);
 		ret = G2DDRM_SUCCESS;
 	} while (false);
 

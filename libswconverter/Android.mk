ed5697dc2fc45357f9a666c1fb19fbcbd9f76087
Alberto96 <albertoguanti@gmail.com>
2015-04-21 16:57:44 +0300

    exynos5-insignal: libswconverter: Add missing includes
    Change-Id: I792f8d9e882eae973960939a6668ab41b9c822cf

--- libswconverter/Android.mk
+++ libswconverter/Android.mk
@@ -13,9 +13,19 @@ LOCAL_SRC_FILES := \
 	csc_ARGB8888_to_YUV420SP_NEON.s \
 	csc_ARGB8888_to_ABGR8888.s
 
+ifeq ($(BOARD_USE_KHRONOS_OMX_HEADER),true)
+LOCAL_C_INCLUDES += $(TOP)/hardware/samsung_slsi/openmax/include/khronos
+else
+LOCAL_C_INCLUDES += $(TOP)/frameworks/native/include/media/openmax
+endif
+
 LOCAL_C_INCLUDES := \
-	$(TOP)/hardware/samsung_slsi/openmax/include/khronos \
-	$(LOCAL_PATH)/../include
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
+	$(LOCAL_PATH)/../include \
+	$(TOP)/hardware/samsung_slsi/exynos/include
+
+LOCAL_ADDITIONAL_DEPENDENCIES := \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_MODULE := libswconverter
 
